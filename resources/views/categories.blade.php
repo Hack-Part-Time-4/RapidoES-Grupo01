196b8a6d28572a7aed905609ea0895eaf90ccb70
Ignacio Gutierrez <ignagutz94@gmail.com>
2022-12-16 22:05:35 +0100

    commit en rama nueva

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <h1 class="text-center mt-5">EN MANTENIMIENTO ...</h1>
+    <h1 class="text-center mt-5">EN MANTENIMIENTO .....</h1>
     <div class="text-center mt-5">
     <img src="{{asset ('imgs/mant.png')}}" width="25%" alt="">
     </div>
