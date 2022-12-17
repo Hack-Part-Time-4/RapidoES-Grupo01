89f2cb9ae924bfa13ff2a3cffc3d2e8b40acabe3
Ignacio Gutierrez <ignagutz94@gmail.com>
2022-12-17 15:15:00 +0100

    editamos el archivo create de livewire de prueba

--- resources/views/livewire/create-ad.blade.php
+++ resources/views/livewire/create-ad.blade.php
@@ -1,3 +1,8 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, asperiores? Ratione quo consectetur laborum.
+        Optio veniam nesciunt vitae ut quisquam, consequatur nostrum amet numquam iste magni accusantium ducimus est
+        quaerat!
+        
+    </p>
 </div>
