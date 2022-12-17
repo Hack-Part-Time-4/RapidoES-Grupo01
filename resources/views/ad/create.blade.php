8b637e20b6adbdcf8c0be1e0d3bb175ba8b684f4
Ignacio Gutierrez <ignagutz94@gmail.com>
2022-12-17 14:58:04 +0100

    Añadimos la carta del anuncio en la vista

--- resources/views/ad/create.blade.php
+++ resources/views/ad/create.blade.php
@@ -2,6 +2,18 @@
     <x-slot name="title">
         Rapido - Añadir anuncio
     </x-slot>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-8">
+                <div class="card text-bg-info border-dark ">
+                    <div class="card-header text-center">
+                        <h4>Nuevo anuncio</h4>
+                    </div>
+                    <div class="card-body">
+                      This is some text within a card body.
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
-
-<h1>ACA SE VA CREAR EL ANUNCIO</h1>
