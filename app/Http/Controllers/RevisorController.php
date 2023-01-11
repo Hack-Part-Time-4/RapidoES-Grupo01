2e38fe550ca1f573d9d098f1c96d3f7d6d4f239e
Ignacio Gutierrez <ignagutz94@gmail.com>
2023-01-11 18:31:17 +0100

    aceptar y rechazar anuncio

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -5,21 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Ad;
 use Illuminate\Http\Request;
 
+use function PHPSTORM_META\type;
+
 class RevisorController extends Controller
 {
     public function index()
     {
-        $ad = Ad::where('is_accepted',false)->orderBy('created_at','desc')->first();
+        $ad = Ad::where('is_accepted',null)->orderBy('created_at','desc')->first();
         return view('revisor.home',compact('ad'));
     }
 
-    public function acceptAd()
+    public function acceptAd(Ad $ad)
     {
-        dd("Solo para revisores");
+        $ad->is_accepted = true;
+        $ad->save();
+        return redirect()->back()->withMessage(['type'=>'success','text'=>'Anuncio aceptado']);
     }
 
-    public function rejectAd()
+    public function rejectAd(Ad $ad)
     {
-        dd("Solo para revisores");
+        $ad->is_accepted = false;
+        $ad->save();
+        return redirect()->back()->withMessage(['type'=>'danger','text'=>'Anuncio no aceptado']);
     }
 }
