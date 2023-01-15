c119e7091714220b8aff20858feb9d4b5e5edda4
Ignacio Gutierrez <ignagutz94@gmail.com>
2023-01-15 12:34:17 +0100

    Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Work with Us</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+      </ul>
+      <ul class="nav justify-content-center pb-3 mb-3">
+        <li class="text-center text-muted">© 2022 Company, Inc</li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use href="#twitter"><i class="bi bi-twitter"></i></use></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use href="#instagram"><i class="bi bi-instagram"></i></use></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use href="#facebook"><i class="bi bi-facebook"></i></use></svg></a></li>
+      </ul>
+      
+    </footer>
