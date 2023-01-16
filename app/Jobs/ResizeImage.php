94de5924e15af597733946b950a4ab24fcac6804
Ignacio Gutierrez <ignagutz94@gmail.com>
2023-01-16 12:21:38 +0100

    Añadimos Spatie/image

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -2,7 +2,9 @@
 
 namespace App\Jobs;
 
+use Spatie\Image\Image;
 use Illuminate\Bus\Queueable;
+use Spatie\Image\Manipulations;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -41,5 +43,7 @@ class ResizeImage implements ShouldQueue
         $srcPath = storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
 
         $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}" . $this->fileName;
+
+        $croppedImage = Image::load($srcPath)->crop(Manipulations::CROP_CENTER, $w, $h)->save($destPath);
     }
 }
