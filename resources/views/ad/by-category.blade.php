381925c0f1203d75632d33ea347ca01cd3c3f0b1
Ignacio Gutierrez <ignagutz94@gmail.com>
2023-01-17 19:04:37 +0100

    User 6 finalizado

--- resources/views/ad/by-category.blade.php
+++ resources/views/ad/by-category.blade.php
@@ -9,12 +9,17 @@
         <div class="row">
             @forelse ($ads as $ad)
 
-
+           {{--  @if ($ad->images()->count() > 0)
+            <img src="{{$ad->images()->first()->getUrl(400,300)}}" class="card-img-top"
+            alt="...">
+            @else
+                <img src="https://picsum.photos/150" class="card-img-top" alt="">
+            @endif --}}
 
             <div class="col-12 col-md-4 mb-5">
                 <div class="card" style="width: 18rem;">
                     @if ($ad->images()->count() > 0)
-                        <img src="{{ Storage::url($ad->images()->first()->path) }}" class="card-img-top" alt="...">
+                        <img src="{{$ad->images()->first()->getUrl(400,300)}}" class="card-img-top" alt="...">
                     @else
                         <img src="https://picsum.photos/150" class="card-img-top" alt="">
                     @endif
