0f7ba9a90e14d0fb0d0e7e0a3c88cae1d9934a23
Ignacio Gutierrez <ignagutz94@gmail.com>
2023-01-21 17:23:34 +0100

    more details

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,18 +1,18 @@
 
-<footer class="py-3 my-4">
-      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-        <li class="nav-item"><a href="{{ route('revisor.become') }}" class="nav-link px-2 text-muted">Work with Us</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+<footer class="py-3 my-4 backgroundNavbar ">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3 ">
+        <li class="nav-item "><a href="#" class="nav-link px-2 navText">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 navText">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 navText">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 navText">FAQs</a></li>
+        <li class="nav-item"><a href="{{ route('revisor.become') }}" class="nav-link px-2 loginColor">Work with Us</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 navText">About</a></li>
       </ul>
       <ul class="nav justify-content-center pb-3 mb-3">
-        <li class="text-center text-muted">© 2023 Company, Inc</li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use href="#twitter"><i class="bi bi-twitter"></i></use></svg></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use href="#instagram"><i class="bi bi-instagram"></i></use></svg></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use href="#facebook"><i class="bi bi-facebook"></i></use></svg></a></li>
+        <li class="text-center navText">© 2023 Company, Inc</li>
+      <li class="ms-3"><a class="navText" href="#"><svg class="bi" width="24" height="24"><use href="#twitter"><i class="bi bi-twitter"></i></use></svg></a></li>
+      <li class="ms-3"><a class="navText" href="#"><svg class="bi" width="24" height="24"><use href="#instagram"><i class="bi bi-instagram"></i></use></svg></a></li>
+      <li class="ms-3"><a class="navText" href="#"><svg class="bi" width="24" height="24"><use href="#facebook"><i class="bi bi-facebook"></i></use></svg></a></li>
       </ul>
       
     </footer>
