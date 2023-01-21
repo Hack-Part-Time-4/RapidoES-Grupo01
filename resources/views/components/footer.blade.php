be62d3bc0f22d94e4af6f76f917e43bf81ad9006
elbricios <elbricios@gmail.com>
2023-01-21 18:47:33 +0100

    puesta al dia

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,12 +5,12 @@
         <li class="nav-item"><a href="#" class="nav-link px-2 navText">Features</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-2 navText">Pricing</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-2 navText">FAQs</a></li>
-        <li class="nav-item"><a href="{{ route('revisor.become') }}" class="nav-link px-2 loginColor">Work with Us</a></li>
+        <li class="nav-item"><a href="{{ route('revisor.become') }}" class="nav-link px-2 loginColor ">Work with Us</a></li>
         <li class="nav-item"><a href="#" class="nav-link px-2 navText">About</a></li>
       </ul>
       <ul class="nav justify-content-center pb-3 mb-3">
         <li class="text-center navText">© 2023 Company, Inc</li>
-      <li class="ms-3"><a class="navText" href="#"><svg class="bi" width="24" height="24"><use href="#twitter"><i class="bi bi-twitter"></i></use></svg></a></li>
+      <li class="ms-3"><a class="navText text-warning" href="#"><svg class="bi" width="24" height="24"><use href="#twitter"><i class="bi bi-twitter"></i></use></svg></a></li>
       <li class="ms-3"><a class="navText" href="#"><svg class="bi" width="24" height="24"><use href="#instagram"><i class="bi bi-instagram"></i></use></svg></a></li>
       <li class="ms-3"><a class="navText" href="#"><svg class="bi" width="24" height="24"><use href="#facebook"><i class="bi bi-facebook"></i></use></svg></a></li>
       </ul>
