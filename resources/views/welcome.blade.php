9df523e8120bcdbdb379236d841c383c08222bdd
Ignacio Gutierrez <ignagutz94@gmail.com>
2023-01-22 19:19:51 +0100

    Se añadio la traduccion base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
     <div class="container mt-3">
         <div class="row mb-5">
-            <div class="col-12 text-center  h1Color">
+            <div class="col-12 text-center ">
                 <h1>{{__('messages.welcome')}}</h1>
             </div>
         </div>
