6bf2bd804ef5258dcd9b50959f1befec6860e290
elbricios <elbricios@gmail.com>
2023-01-23 18:23:42 +0100

    categorias movil

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,27 +1,17 @@
 <x-layout>
     <x-slot name='title'>Rapido - {{__('Categorías')}}</x-slot>
 
-    <div class="container mt-3">
-        <div class="row">
-            <div class="col-12">
-                <h1>{{__('Listado de categorías')}}</h1>
-            </div>
-        </div>
-    </div>
+
+    <h1 class="text-center mt-3">{{__('Listado de categorías')}}</h1>
+
 
     @forelse ($cats as $cat)
-    <div class="container">
-        <div class="row justify-content-center mt-4">
-                <div class="list-group">
-                    <a href="{{ route('category.ads',$cat) }} "
-                        class="list-group-item list-group-item-primary btn btn-primary p-4" >
-                        {{ $cat->name }}
-                    </a>
-                </div>
-                @empty
-
-                @endforelse
-            </div>
-        </div>
+    <div class="list-group my-3 mx-5">
+        <a href="{{ route('category.ads',$cat) }} "
+            class="{{-- list-group-item list-group-item-primary --}} btn btn-primary">
+            {{ $cat->name }}
+        </a>
     </div>
+    @empty
+    @endforelse
 </x-layout>
