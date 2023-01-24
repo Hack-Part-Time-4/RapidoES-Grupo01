a9b797bf07266974c1fc1e9221a15679b2acb553
elbricios <elbricios@gmail.com>
2023-01-24 18:14:05 +0100

    fin

--- resources/views/ad/show.blade.php
+++ resources/views/ad/show.blade.php
@@ -1,50 +1,48 @@
 <x-layout>
-    <x-slot name='title'>Rapido - {{ $ad->title }}</x-slot>
 
-    <div class="container mt-3">
-
-        <div class="row mb-5">
-            <div class="col-12">
-                <h1>{{__('Detalle de anuncio')}}: {{ $ad->title }}</h1>
-            </div>
-        </div>
-
-        <div class="row">
-           <div class="col-12 col-md-4 mb-5">
-                <div class="card" style="width: 18rem;">
-                    @if ($ad->images()->count() > 0)
-                        <img src="{{$ad->images()->first()->getUrl(400,300)}}" class="card-img-top img-fluid" alt="...">
-                    @else
-                        <img src="https://picsum.photos/150" class="card-img-top img-fluid" alt="">
-                    @endif
-                    <div class="card-body">
-                        <h5 class="card-title pb-2">{{$ad->title}}</h5>
-
-                        <div class="card-subtitle pb-2 text-primary">
-                            <a href="{{ route('category.ads',$ad->category) }}"
-                                class="btn btn-info">{{$ad->category->name}}</a>
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-12 col-md-6">
+                <div id="adImages" class="carousel slide" data-bs-ride="true">
+                    <div class="carousel-indicators">
+                        @for ($i = 0; $i < $ad->images()->count(); $i++)
+                        <button type="button" data-bs-target="#adImages" data-bs-slide-to="{{$i}}" class="@if($i == 0) active @endif" aria-current="true" aria-label="Slide {{$i + 1}}"></button>
+                        @endfor
+                    </div>
+                    <div class="carousel-inner">
+                        @foreach ($ad->images as $image)
+                        <div class="carousel-item @if($loop->first) active @endif">
+                            <img src="{{ $image->getUrl(400,300) }}" class="d-block w-100" alt="...">
                         </div>
-
-                        <p class="card-text">{{$ad->body}}</p>
-
-                        <div class="d-flex justify-content-between pb-3">
-                            <h5 class="card-subtitle">€{{$ad->price}}</h5>
-                            <h5 class="card-subtitle">{{$ad->created_at->format('d/m/Y')}}</h5>
-                        </div>
-
-                        <div class="card-subtitle">
-                            <small>{{$ad->user->name}}</small>
-                        </div>
-
-
+                        @endforeach
                     </div>
 
+                    <button class="carousel-control-prev" type="button" data-bs-target="#adImages" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#adImages" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
-
             </div>
 
-        </div>
-    </div>
+            <div class="col-12 col-md-6">
+                <div><b>{{__("Título")}}:</b> {{$ad->title}}</div>
+                <div><b> {{__("Precio")}}:</b> {{$ad->price}}</div>
+                <div><b> {{__("Descripción")}}:</b> {{$ad->body}}</div>
+                <div><b> {{__("Publicado el")}}:</b> {{$ad->created_at->format('d/m/Y')}}</div>
+                <div><b> {{__("Por")}}:</b> {{$ad->user->name}}</div>
 
+                <div>
+                    <a href="{{route('category.ads',$ad->category)}}">#{{$ad->category->name}}</a></div>
+                <div>
+                    <a href="#" class="btn btn-outline-success">{{ __("Comprar") }}</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
 </x-layout>
