e348545a15df2cc841f85034b3d8c7e9bacc67a4
elbricios <elbricios@gmail.com>
2023-01-24 18:06:21 +0100

    todos juntos

--- resources/views/revisor/home.blade.php
+++ resources/views/revisor/home.blade.php
@@ -11,7 +11,7 @@
 
                     <div class="row">
                         <div class="col-12 col-md-8 offset-md-2">
-                            <div class="card">
+                            <div class="card backgroundCard">
                                 <div class="card-header">
                                     Anuncio #{{$ad->id}}
                                 </div>
@@ -85,10 +85,8 @@
                                 </div>
                             </div>
                         </div>
-
-
                     </div>
-                   
+            
                 </div>
             </div>
 
